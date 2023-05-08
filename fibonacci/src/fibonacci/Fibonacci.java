21d0e7330e274b0b27712efd1a1ca2c92ac2ff10
Barış Savak <128433645+bariss30@users.noreply.github.com>
2023-05-08 16:46:56 +0300

    Add files via upload

---
+++ fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,36 @@
+
+package fibonacci;
+
+
+public class Fibonacci {
+
+   
+    public static void main(String[] args) {
+     int s1,s2,temp;
+     s1=0;
+     s2=1;
+        System.out.println(s1);
+        System.out.println(s2);
+do
+{
+    temp=s1;
+    s1=s2;
+    s2=temp+s2;
+    if(s2>=1000){
+    break;
+    }
+    
+    System.out.println(s2);
+
+   
+} while(s2<1000);
+     
+
+
+        System.out.println("barsi");
+     
+        
+        
+            }
+    
+}
